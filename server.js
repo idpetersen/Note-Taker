da1e8b728e773d30c4356fc00d2d28c6b4524cc0
isaac petersen <isaac.petersen5@gmail.com>
2022-01-28 15:31:36 -0800

    added routes and new notes are being posted to db

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
+const { v4: uuidv4 } = require('uuid');
 
 const PORT = process.env.PORT || 3001;
 
@@ -9,15 +11,40 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
-//Get route for notes.html
+
+//Get route for api/notes
+
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.get('/api/notes', (req, res) =>{
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  });
+
+
+app.post('/api/notes', (req, res)=>{
+    const {title, text} = req.body;
+
+    if(req.body){
+        const newNote = {
+            title,
+            text,
+            noteID: uuidv4(),
+        }
+
+        readAndAppend(newNote, './db/db.json');
+        res.json('Added new note');
+    } else {
+        res.error('Error in adding note')
+    }
+})  
 //Get route for index.html
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:PORT`)
+  console.log(`App listening at http://localhost:${PORT}`)
 );
+
