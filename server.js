2d670f0754cec26a26f6dbf13a82e1554cc8e15e
isaac petersen <isaac.petersen5@gmail.com>
2022-01-29 19:47:33 -0800

    launching

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
+//Importing the required packages 
 const express = require('express');
 const path = require('path');
 const {
     readFromFile,
-    readAndAppend,
-    writeToFile
+    readAndAppend
 } = require('./helpers/fsUtils');
 const {
     v4: uuidv4
@@ -21,30 +21,31 @@ app.use(express.urlencoded({
 
 app.use(express.static('public'));
 
-//Get route for api/notes
-
+//Get route for notes
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
-
+//Get route for notes/api
 app.get('/api/notes', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-
+//Post route for adding notes
 app.post('/api/notes', (req, res) => {
+    //Destructuring the request body to set the title and text
     const {
         title,
         text
     } = req.body;
 
     if (req.body) {
+        //Setting new title content, text, content, and assinging a new unique id using the uuid package and uuidv4 method
         const newNote = {
             title,
             text,
             id: uuidv4(),
         }
-
+        //Adding new note with the read and append method
         readAndAppend(newNote, './db/db.json');
         res.json('Added new note');
     } else {
@@ -52,41 +53,35 @@ app.post('/api/notes', (req, res) => {
     }
 })
 
-
+//Delete route for deleting notes
 app.delete("/api/notes/:id", (req, res) => {
     fs.readFile("./db/db.json", (err, data) => {
         const notes = JSON.parse(data);
         if (err) {
             throw err
         } else {
+            //Filtering out the matching ID from the request and returning the DB without the matching ID note
             const filteredNotes = notes.filter((note) => {
                 return note.id !== req.params.id
             });
+            //Overwriting the existing db without the filtered note
             fs.writeFile("./db/db.json", JSON.stringify(filteredNotes, null, 4), (err) => {
                 if (err) {
                     throw err
                 } else {
-                    return res.json(notes)
+                    return res.json(filteredNotes)
                 }
             });
         }
     });
 });
 
-
 //Get route for index.html
 app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+//Connecting to port
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
 );
-
-// const getId = () => {
-//     readFromFile('./db/db.json').then((data) => {
-//         for (let i = 0; index < data.length; index++) {
-//             const id = data[i].id  
-//         }
-//     })
-// }
